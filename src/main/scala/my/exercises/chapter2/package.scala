09d6f6ca4a68cf930ec0a4a3b4845e6b6c2dd173
Igor Levoniuk <igor.levoniuk@gmail.com>
2015-03-10 23:53:37 +0200

    Chapter 2: curry(), uncurry() and compose().

--- src/main/scala/my/exercises/chapter2/package.scala
+++ src/main/scala/my/exercises/chapter2/package.scala
@@ -19,5 +19,12 @@ package object chapter2 {
     go(1, res = true)
   }
 
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    a => f(a, _)
 
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }
